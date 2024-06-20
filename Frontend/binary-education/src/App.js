4dcff86c87dbdb544d3da93084364bbf04466610
ARPIT <arpitdude006@gmail.com>
2024-06-20 16:55:00 +0530

    4th

--- Frontend/binary-education/src/App.js
+++ Frontend/binary-education/src/App.js
@@ -4,12 +4,11 @@ import Homepage from './Homepage/Homepage';
 import Navbar from './Components/MainNavbar';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Book from './Components/Book';
-import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+   
       <Router>
         <Routes>
           <Route path='/' element={<Homepage/>}></Route>
